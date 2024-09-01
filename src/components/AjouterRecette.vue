9cff538b0f971d854690a9f6f000b6b19384ba4a
AbderahmaneThimbo <abderahmanethimbo@gmail.com>
2024-09-01 14:43:08 +0000

    composant d'ajout

---
+++ src/components/AjouterRecette.vue
@@ -0,0 +1,62 @@
+<template>
+    <div>
+      <h3 class="text-center fw-bold mt-4">Ajouter une nouvelle Recette</h3>
+      <form @submit.prevent="store.addRecette(nouvelleRecette)">
+        <div class="mb-3">
+          <label for="titre" class="form-label">Titre</label>
+          <input
+            type="text"
+            class="form-control"
+            id="titre"
+            placeholder="Entrer le titre de la recette"
+            v-model="nouvelleRecette.titre"
+            required
+          />
+        </div>
+        <div class="mb-3">
+          <label for="ingredient" class="form-label">Ingrédients</label>
+          <textarea
+            class="form-control"
+            id="ingredient"
+            placeholder="Entrer les ingrédients de la recette"
+            v-model="nouvelleRecette.ingredient"
+            required
+          ></textarea>
+        </div>
+        <div class="mb-3">
+          <label for="type" class="form-label">Type de recette</label>
+          <select class="form-select" id="type" v-model="nouvelleRecette.type" required>
+            <option value="Entrée">Entrée</option>
+            <option value="Plat">Plat</option>
+            <option value="Dessert">Dessert</option>
+          </select>
+        </div>
+        <RouterLink type="submit" class="btn btn-primary w-25" to="/liste">Enregistrer
+        </RouterLink> 
+      </form>
+    </div>
+  </template>
+  
+  <script setup>
+  import { ref } from "vue";
+  import { useRecetteStore } from "@/stores/recette";
+  
+  const store = useRecetteStore();
+  
+  const nouvelleRecette = ref({
+    titre: "",
+    ingredient: "",
+    type: "",
+  });
+  
+  const ajouterRecette = () => {
+    store.addRecette({ ...nouvelleRecette.value });
+  
+    nouvelleRecette.value = {
+      titre: "",
+      ingredient: "",
+      type: "",
+    };
+  };
+  </script>
+  
